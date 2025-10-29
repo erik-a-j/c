1fca0e4f7279ba57dd92b92f7823f2db4ef75320
Erik Johansson <erik.alexius.johansson@gmail.com>
2025-10-29 11:02:54 +0100

    w44

--- exercises/w44/31_count_digit.c
+++ exercises/w44/31_count_digit.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int numlen(int num) {
-
-    int len = numlen(num);
+    int a = (num < 0) ? -num : num;
+    return (a < 10) ? 1 : (1 + numlen(a / 10));
 }
 
+static float power(int base, int exp) {
+    int temp = (exp < 0) ? -exp : exp;
+    float result = (temp == 0) ? 1 : (base * power(base, temp - 1));
+    return (exp < 0) ? (1 / result) : result;
+}
+
+#define pnumlen(num) printf(#num " len: %d\n", numlen(num))
 int main(void) {
+    printf("5 ^ 3 == %.2f\n", power(5, 3));
     return 0;
 }
